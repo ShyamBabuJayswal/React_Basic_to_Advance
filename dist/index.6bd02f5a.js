c2b721b68aa4eb202fa831ce6c19125fa2ee432f
NIhal <jaisnj541@gmail.com>
2024-10-05 17:39:46 +0530

    Created our own react app

--- dist/index.6bd02f5a.js
+++ dist/index.6bd02f5a.js
@@ -1,3 +1,4 @@
+i;
 //    const heading=React.createElement("h1",{id:"heading", xyz:"abcd"},"Hello world from React!");
 //    console.log(heading); // return object
 //    const root=ReactDOM.createRoot(document.getElementById("root"));
