68f2519abf8f8962d1817ec02afde93df527f509
NIhal <jaisnj541@gmail.com>
2024-10-05 16:49:50 +0530

    Execute parcel using npx

--- App.js
+++ App.js
@@ -22,33 +22,33 @@
 
 //  </div>
 
-// const parent = React.createElement(
-//     "div",
-//     { id: "parent" },
-//     [
-//       React.createElement(
-//         "div",
-//         { id: "child1" },
-//         [
-//           React.createElement("h1", {}, "I am h1 tag"),
-//           React.createElement("h2", {}, "I am h2 tag")
-//         ]
-//       ),
-//       React.createElement(
-//         "div",
-//         { id: "child2" },
-//         [
-//           React.createElement("h1", {}, "I am h1 tag"),
-//           React.createElement("h2", {}, "I am h2 tag")
-//         ]
-//       )
-//     ]
-//   );
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    [
+      React.createElement(
+        "div",
+        { id: "child1" },
+        [
+          React.createElement("h1", {}, "I am h1 tag"),
+          React.createElement("h2", {}, "I am h2 tag")
+        ]
+      ),
+      React.createElement(
+        "div",
+        { id: "child2" },
+        [
+          React.createElement("h1", {}, "I am h1 tag"),
+          React.createElement("h2", {}, "I am h2 tag")
+        ]
+      )
+    ]
+  );
   
-//   console.log(parent);//object that is react element that broswer understand
+  console.log(parent);//object that is react element that broswer understand
   
-//   const root=ReactDOM.createRoot(document.getElementById("root"));
-//      root.render(parent);
+  const root=ReactDOM.createRoot(document.getElementById("root"));
+     root.render(parent);
 
 
 
